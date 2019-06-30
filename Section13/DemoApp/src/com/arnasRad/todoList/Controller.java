ef228e97cd69a1fc3d6d730a709faf72f0652b65
arnasRad <arnasrad@gmail.com>
2019-06-30 15:56:50 +0300

    post video 190 commit. Context menu. Delete an item from the list

--- Section13/DemoApp/src/com/arnasRad/todoList/Controller.java
+++ Section13/DemoApp/src/com/arnasRad/todoList/Controller.java
@@ -4,6 +4,8 @@ import com.arnasRad.todoList.data_model.TodoData;
 import com.arnasRad.todoList.data_model.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
@@ -31,6 +33,9 @@ public class Controller {
     @FXML
     private BorderPane mainBorderPane;
 
+    @FXML
+    private ContextMenu listContextMenu;
+
     public void initialize() {
 //        TodoItem item1 = new TodoItem("Mail birthday card",
 //                "Buy a 30th birthday card for John",
@@ -56,6 +61,17 @@ public class Controller {
 //        todoItems.add(item5);
 //
 //        TodoData.getInstance().setTodoItems(todoItems);
+
+        listContextMenu = new ContextMenu();
+        MenuItem deleteMenuItem = new MenuItem("Delete");
+        deleteMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                deleteItem(item);
+            }
+        });
+        listContextMenu.getItems().addAll(deleteMenuItem);
 
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
@@ -92,6 +108,17 @@ public class Controller {
                         }
                     }
                 };
+
+                cell.emptyProperty().addListener(
+                        (obs, wasEmpty, isNowEmpty) -> {
+                                if(isNowEmpty) {
+                                    cell.setContextMenu(null);
+                                } else {
+                                    cell.setContextMenu(listContextMenu);
+                                }
+                        }
+                );
+
                 return cell;
             }
         });
@@ -139,5 +166,17 @@ public class Controller {
 //        sb.append(item.getDeadline().toString());
 //
 //        itemDetailsTextArea.setText(sb.toString());
+    }
+
+    public void deleteItem(TodoItem item) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete Todo item");
+        alert.setHeaderText("Delete item: " + item.getShortDescription());
+        alert.setContentText("Are you sure? Press OK to confirm, or cancel to Back out.");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && (result.get() == ButtonType.OK)) {
+            TodoData.getInstance().deleteTodoItem(item);
+        }
     }
 }
