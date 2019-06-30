72ae94063fdc882289813c392038dbcf036a00de
arnasRad <arnasrad@gmail.com>
2019-06-30 22:57:56 +0300

    Section 13 video 196. More on Choosers and Web Pages

--- Section13/JavaFX_Application/src/sample/Controller.java
+++ Section13/JavaFX_Application/src/sample/Controller.java
@@ -8,7 +8,12 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 public class Controller {
     @FXML
@@ -38,14 +43,38 @@ public class Controller {
 
     @FXML
     public void handleClick() {
-//        FileChooser chooser = new FileChooser();
-        DirectoryChooser chooser = new DirectoryChooser();
-//        chooser.showOpenDialog(gridPane.getScene().getWindow());
-        File file = chooser.showDialog(gridPane.getScene().getWindow());
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Save Application File");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text", "*.txt"),
+                new FileChooser.ExtensionFilter("PDF", "*.pdf"),
+                new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.gif"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+        );
+
+//        DirectoryChooser chooser = new DirectoryChooser();
+        List<File> file = chooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
+//        File file = chooser.showDialog(gridPane.getScene().getWindow());
         if (file != null) {
-            System.out.println(file.getPath());
+            for(int i = 0; i < file.size(); ++i) {
+                System.out.println(file.get(i));
+            }
+//            System.out.println(file.getPath());
         } else {
             System.out.println("Chooser was cancelled");
+        }
+    }
+
+    @FXML
+    public void handleLinkClick() {
+        try {
+            Desktop.getDesktop().browse(new URI("http://www.javafx.com"));
+        } catch (IOException e) {
+            System.err.println("IO exception");
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            System.err.println("URI exception");
+            e.printStackTrace();
         }
     }
 }
