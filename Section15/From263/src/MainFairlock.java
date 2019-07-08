bfa29e0aff14cab2c645ca9daa9293dd007afd3e
arnasRad <arnasrad@gmail.com>
2019-07-08 15:49:37 +0300

    end of video 265

--- Section15/From263/src/MainFairlock.java
+++ Section15/From263/src/MainFairlock.java
@@ -1,7 +1,9 @@
 import com.timbuchalka.ThreadColor;
 
-public class Main {
-    private static Object lock = new Object();
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MainFairlock {
+    private static ReentrantLock lock = new ReentrantLock(true); // true -> FCFS order
 
     public static void main(String[] args) {
         Thread t1 = new Thread(new Worker(ThreadColor.ANSI_RED), "Priority 10");
@@ -10,11 +12,11 @@ public class Main {
         Thread t4 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 4");
         Thread t5 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 2");
 
-//        t1.setPriority(10);
-//        t2.setPriority(8);
-//        t3.setPriority(6);
-//        t4.setPriority(4);
-//        t5.setPriority(2);
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
 
         t1.start();
         t2.start();
@@ -34,9 +36,12 @@ public class Main {
         @Override
         public void run() {
             for(int i = 0; i < 100; ++i) {
-                synchronized (lock) {
+                lock.lock();
+                try {
                     System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName(), runCount++);
                     // execute critical section of code
+                } finally {
+                    lock.unlock();
                 }
             }
         }
