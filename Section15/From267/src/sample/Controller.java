5d2096a5f85aba8000d173b689aacae507d126ee
arnasRad <arnasrad@gmail.com>
2019-07-08 16:12:32 +0300

    end of video 268

--- Section15/From267/src/sample/Controller.java
+++ Section15/From267/src/sample/Controller.java
@@ -5,11 +5,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 public class Controller {
     private Task<ObservableList<String>> task;
     @FXML private ListView listView;
+    @FXML private ProgressBar progressBar;
+    @FXML private Label progressLabel;
 
     public void initialize() {
         task = new Task<ObservableList<String>>() {
@@ -17,27 +21,30 @@ public class Controller {
             protected ObservableList<String> call() throws Exception {
                 Thread.sleep(1000);
 
-                ObservableList<String> employees = FXCollections.observableArrayList(
+                String[] names = {
                         "Tim Buchalka",
-                            "Bill Rogers",
-                            "Jack Jill",
-                            "Joan Andrews",
-                            "Mary Johnson",
-                            "Bob McDonald"
-                        );
+                        "Bill Rogers",
+                        "Jack Jill",
+                        "Joan Andrews",
+                        "Mary Johnson",
+                        "Bob McDonald"
+                };
 
-                // a bad practice. Rather use data binding
-//                Platform.runLater(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        listView.setItems(employees);
-//                    }
-//                });
+                ObservableList<String> employees = FXCollections.observableArrayList();
+
+                for(int i = 0; i < 6; ++i) {
+                    employees.add(names[i]);
+                    updateMessage("Added " + names[i] + " to the list");
+                    updateProgress(i + 1, 6);
+                    Thread.sleep(200);
+                }
 
                 return employees;
             }
         };
 
+        progressLabel.textProperty().bind(task.messageProperty());
+        progressBar.progressProperty().bind(task.progressProperty());
         listView.itemsProperty().bind(task.valueProperty());
     }
 
