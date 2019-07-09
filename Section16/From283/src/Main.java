4c941a5ec464c5894713f052fb0bf9b1957ace2b
arnasRad <arnasrad@gmail.com>
2019-07-09 23:55:37 +0300

    Video 285. flatMap() method

--- Section16/From283/src/Main.java
+++ Section16/From283/src/Main.java
@@ -41,5 +41,26 @@ public class Main {
                 .distinct()
                 .peek(System.out::println)  // mainly used to support debugging
                 .count());
+
+        System.out.println();
+        Employee john = new Employee("John Doe", 30);
+        Employee jane = new Employee("Jane Deer", 25);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+
+        Department hr = new Department("Human Resources");
+        hr.addEmployee(jane);
+        hr.addEmployee(jack);
+        hr.addEmployee(snow);
+        Department accounting = new Department("Accounting");
+        accounting.addEmployee(john);
+
+        List<Department> departments = new ArrayList<>();
+        departments.add(hr);
+        departments.add(accounting);
+
+        departments.stream()
+                .flatMap(department -> department.getEmployees().stream())
+                .forEach(System.out::println);
     }
 }
