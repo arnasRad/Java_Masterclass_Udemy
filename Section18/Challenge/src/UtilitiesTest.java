228e44ecd1576fe346d61ab584d359b0fb9543a3
arnasRad <arnasrad@gmail.com>
2019-07-12 10:10:15 +0300

    Challenge #1 completed

---
+++ Section18/Challenge/src/UtilitiesTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+
+public class UtilitiesTest {
+
+    @Test
+    public void everyNthChar() {
+        fail("This test has not been implemented");
+    }
+
+    @Test
+    public void removePairs() {
+        fail("This test has not been implemented");
+    }
+
+    @Test
+    public void converter() {
+        fail("This test has not been implemented");
+    }
+
+    @Test
+    public void nullIfOddLength() {
+        fail("This test has not been implemented");
+    }
+}
