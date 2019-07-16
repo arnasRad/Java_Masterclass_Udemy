283a3da3ebc48d7de7410a8c3aa9f9b614d7fb9b
arnasRad <arnasrad@gmail.com>
2019-07-16 23:03:00 +0300

    End of video 338

---
+++ Section19/From336/src/sample/model/Album.java
@@ -0,0 +1,31 @@
+package sample.model;
+
+public class Album {
+    private int id;
+    private String name;
+    private int artisdId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getArtisdId() {
+        return artisdId;
+    }
+
+    public void setArtisdId(int artisdId) {
+        this.artisdId = artisdId;
+    }
+}
